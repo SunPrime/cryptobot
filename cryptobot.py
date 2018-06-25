aff5c61db2a3038e9adf27b37ec517971574fa53
sunprime <sunprime@ukr.net>
2018-06-25 16:14:18 +0300

    last update8

--- cryptobot.py
+++ cryptobot.py
@@ -5,52 +5,56 @@ import psycopg2.extras
 from binance.client import Client
 from telegram_bot import send_message
 
+def main():
 
-def get_current_price():
-    array_price = {}
-    prices = client.get_all_tickers()
-    for coin in prices:
-        symbol = coin['symbol']
-        price = float(coin['price'])
-        array_price[symbol] = price
-    return array_price
+    def get_current_price():
+        array_price = {}
+        prices = client.get_all_tickers()
+        for coin in prices:
+            symbol = coin['symbol']
+            price = float(coin['price'])
+            array_price[symbol] = price
+        return array_price
 
 
-client = Client(os.environ['BINANCE_APIKEY'], os.environ['BINANCE_SECRET'])
+    client = Client(os.environ['BINANCE_APIKEY'], os.environ['BINANCE_SECRET'])
 
-connect = psycopg2.connect(host=os.environ['DB_HOST'], port=5432, database=os.environ['DB_DATABASE'],
-                           user=os.environ['DB_USER'], password=os.environ['DB_PASSWORD'])
+    connect = psycopg2.connect(host=os.environ['DB_HOST'], port=5432, database=os.environ['DB_DATABASE'],
+                               user=os.environ['DB_USER'], password=os.environ['DB_PASSWORD'])
 
-previous = get_current_price()
-i = 0
-while i < 10:
-    messages = []
+    previous = get_current_price()
+    i = 0
+    while i < 10:
+        messages = []
 
-    current = get_current_price()
+        current = get_current_price()
 
-    result = 0.0
+        result = 0.0
 
-    for key, value in current.items():
-        if value < previous[key]:
-            result = (value - previous[key]) / previous[key]
-            if result < -0.005:
-                value = key + ': ' + str(current[key]) + ' ' + str(round(result * 100, 2)) + '%'
-                messages.append(value)
+        for key, value in current.items():
+            if value < previous[key]:
+                result = (value - previous[key]) / previous[key]
+                if result < -0.005:
+                    value = key + ': ' + str(current[key]) + ' ' + str(round(result * 100, 2)) + '%'
+                    messages.append(value)
 
-    now = int(time.time())
-    sql = ("""insert into binance_bot(time, data) values (%s, %s::json)""")
-    cursore = connect.cursor()
-    cursore.execute(sql, (now, json.dumps(current)))
-    connect.commit()
+        now = int(time.time())
+        sql = ("""insert into binance_bot(time, data) values (%s, %s::json)""")
+        cursore = connect.cursor()
+        cursore.execute(sql, (now, json.dumps(current)))
+        connect.commit()
 
-    print(messages)
+        print(messages)
 
-    send_message(messages)
+        send_message(messages)
 
-    time.sleep(35)
+        time.sleep(10)
 
-    previous = current
-    i += 1
+        previous = current
+        i += 1
 
-connect.close()
-print('exit')
+    connect.close()
+    print('exit')
+
+if __name__ == '__main__':
+    main()
--- telegram_bot.py
+++ telegram_bot.py
@@ -8,12 +8,11 @@ def send_message(msg):
     if len(msg) > 0:
         for message in msg:
             try:
-                urllib.request.urlopen("""
-                https://api.telegram.org/bot{API_TOKEN}/sendMessage?chat_id={CHAT_ID}&text={TEXT}
-                """.format(
+                urllib.request.urlopen("""https://api.telegram.org/bot616413731:{API_TOKEN}/sendMessage?chat_id={CHAT_ID}&text={TEXT}"""
+                    .format(
                 API_TOKEN=os.environ['TELEGRAM_API_TOKEN'],
                 CHAT_ID=os.environ['TELEGRAM_CHAT_ID'],
                 TEXT=message
                 ))
-            except urllib.HTTPError as err:  # timeout error
+            except urllib.error.HTTPError as err:  # timeout error
                 raise MY_URLError("urlopen error", err)
